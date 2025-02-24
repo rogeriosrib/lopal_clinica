7dd3551096147887a09db61a3fccc05aee638f2c
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2025-02-24 17:00:07 -0300

    Começamos os getters e setters

--- src/br/senai/sp/jandira/clinica/ClinicaApp.java
+++ src/br/senai/sp/jandira/clinica/ClinicaApp.java
@@ -9,25 +9,27 @@ public class ClinicaApp {
 	public static void main(String[] args) {
 		
 		Paciente p1 = new Paciente();
-			p1.nome = "Ana Maria";
+			p1.setNome("ana maria");
 			p1.cpf = "999.888.666-77";
-			p1.altura = 1.63;
-			p1.peso = 75.490;
+			p1.setAltura(1.63);
+			p1.setPeso(75);
 			p1.telefone = "(11)96523-8888";
 			p1.dataNascimento = LocalDate.of(2003, 8, 16);
 		
 		Paciente p2 = new Paciente();
-		    p2.nome = "Roger Ribeiro";
+		    p2.setNome("roger ribeiro");
 			p2.cpf = "999.888.222-22";
-			p2.altura = 1.63;
-			p2.peso = 45.200;
+			p2.setAltura(1.20);
+			p2.setPeso(44.0);
 			p2.telefone = "(11)948437615";
 			p2.dataNascimento = LocalDate.of(2008, 4, 5);
 			
 			p1.mostrarDados();
 			p2.mostrarDados();
 					
-					
+			String teste = p1.getNome();
+			System.out.println(teste.toLowerCase());
+			System.out.println(p2.getNome());
 	}
 
 }
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -3,14 +3,49 @@ package br.senai.sp.jandira.clinica.model;
 import java.time.LocalDate;
 
 public class Paciente {
-	
-	public String nome;
+
+	private String nome;
 	public String cpf;
-	public double altura; 
-	public double peso;
+	private double altura;
+	private double peso;
 	public String telefone;
 	public LocalDate dataNascimento;
+
+	public void setAltura(double altura) {
+
+		if (altura > 0 && altura < 3) {
+			// TRUE
+			this.altura = altura;
+		} else {
+			// FALSE
+			System.out.println(
+					"A altura do(a) paciente " + nome + "" + " " + "deve ser maior que ZERO e menor que TRÊS!");
+
+		}
+
+	}
+
+	public void setPeso(double peso) {
+
+		if (peso >= 30 && peso < 500) {
+			// TRUE
+			this.peso = peso;
+		} else {
+			// FALSE
+			System.out.println("O peso do(a) paciente " + nome + " deve ser maior que 30kg e menor que 500kg!");
+		}
+
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+
+	}
 	
+	public String getNome() {
+		return nome;
+	}
+
 	public void mostrarDados() {
 		System.out.println("-----------------");
 		System.out.println("DADOS DO PACIENTE");
@@ -18,11 +53,11 @@ public class Paciente {
 		System.out.println("Nome: " + nome);
 		System.out.println("Peso: " + peso + "kgs");
 		System.out.println("Altura: " + altura + "m");
-		System.out.println("CPF:  " + cpf);	
-		System.out.println("-----------------");	
-		System.out.println("-----------------");	
-		System.out.println("");	
+		System.out.println("CPF:  " + cpf);
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println("");
 
 	}
-	
+
 }
